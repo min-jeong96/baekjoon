154e6f9baa80349e67af37c8e57c7b9bdef83cef
min-jeong96 <minjeong.dev@gmail.com>
2023-01-15 23:36:06 +0900

    [11004] K번째 수 구하기 (퀵 정렬)

---
+++ 11004.js
@@ -0,0 +1,63 @@
+import test from './program.js';
+
+async function func(numbers, index) {
+  let quick = await quickSort(numbers);
+  console.log(quick[index - 1]);
+}
+
+function quickSort(numbers) {
+  let sortedNumbers = sort(numbers);
+
+  if (sortedNumbers.length < 3) {
+    return numbers;
+  } else {
+    let beforePivotIndex = sortedNumbers.findIndex(value => value === numbers[numbers.length]);
+    let left = quickSort(sortedNumbers.slice(0, beforePivotIndex));
+    let right = quickSort(sortedNumbers.slice(beforePivotIndex));
+    
+    let all = left.slice(0);
+    all.push(...right);
+    return all;
+  }
+}
+
+function sort(numbers) {
+  let array = numbers.slice(0, numbers.length - 1);
+  let start = 0;
+  let end = array.length - 1;
+  let pivot = numbers[numbers.length - 1];
+
+  while (end - start > 0) {
+    if (array[start] < pivot) {
+      start++;
+    }
+
+    if ((array[start] > pivot && array[end] < pivot) ||
+        (end - 1 === start && array[start] > array[end])) {
+      let temp = array[end];
+      array[end] = array[start];
+      array[start] = temp;
+    }
+
+    if (array[end] > pivot) {
+      end--;
+    }
+
+    if ((array[start] > pivot && array[end] < pivot) ||
+        (end - 1 === start && array[start] > array[end])) {
+      let temp = array[end];
+      array[end] = array[start];
+      array[start] = temp;
+    }
+  }
+
+  if (array[end] < pivot) {
+    array.splice(end + 1, 0, pivot);
+  } else {
+    array.splice(end, 0, pivot);
+  }
+
+  return array;
+}
+
+await test(func, [[4, 1, 2, 3, 5], 2]);
